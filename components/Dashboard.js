d7ade1467ddb57a3ab32dd4f1f8ec072adf1652e
Dippy <tdipietro87@gmail.com>
2025-06-05 03:18:26 -0400

    Fixed table id prop on skip invite

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -114,7 +114,7 @@ export default function Dashboard() {
   };
 
   const handleSkipInvite = () => {
-    socket.emit('skip_invite', { userId: user._id });
+    socket.emit('skip_invite', { userId: user._id, tableId: invitedTableId });
     clearInterval(inviteIntervalRef.current);
     setInviteModalVisible(false);
     setInvitedTableId(null);
